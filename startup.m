98e4f5c1bc50106f2bc223019aba4dd4ec911a0f
Cedric Klinkert <cedrick@ethz.ch>
2017-02-26 09:54:19 +0100

    initialise

--- startup.m
+++
@@ -1,7 +0,0 @@
-addpath /home/msc15h8/Documents/MATLAB/
-%addpath /home/msc15h8/Documents/MATLAB/wannier/
-%addpath /home/msc15h8/Documents/MATLAB/wannier/2contacts/
-%addpath /home/msc15h8/Documents/MATLAB/vasplab/
-% format long
-% cd /usr/scratch/cungieri/VASP
-% cd /usr/scratch/dahu3/ducryf/vasp/hBN/
